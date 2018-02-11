b304577a81be92cbfd04e1016d36f0b585ecb154
jack yu <yuhaiyang@autohome.com.cn>
2018-02-11 12:48:11 +0800

    JMH microBeanchmark
    JMH microBeanchmark

---
+++ benchmarkdemo/src/main/java/demo/MyBenchmark.java
@@ -0,0 +1,34 @@
+package demo;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+/**
+ * @Author: jack-yu
+ * @Description:
+ */
+public class MyBenchmark {
+    public MyBenchmark() {
+    }
+
+    @Benchmark
+    public void testMethod() {
+        // This is a demo/sample template for building your JMH benchmarks. Edit as needed.
+        // Put your benchmark code here.
+
+        int a = 1;
+        int b = 2;
+        int sum = a + b;
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(MyBenchmark.class.getSimpleName())
+                .forks(1)
+                .build();
+        new Runner(opt).run();
+    }
+}
